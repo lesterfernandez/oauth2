87748e764daae91fa3a9f0b96334ad7f697f2322
Enzo Falone <enzofalone123@gmail.com>
2025-02-08 19:21:37 -0800

    implemented /oauth/exchange, added env vars (#2)

--- example/server/src/routers/oauth-router.ts
+++ example/server/src/routers/oauth-router.ts
@@ -4,12 +4,15 @@ import { Router } from "express";
 
 export const oauthRouter = Router();
 
-oauthRouter.get("/exchange", (req, res) => {
-  if (!req.query["code"] || typeof req.query["code"] !== "string") {
-    res.status(HttpCode.BadRequest).send("Missing code");
-    return;
+oauthRouter.post("/exchange", async (req, res) => {
+  const requiredFields = ["code", "client_id", "redirect_uri", "grant_type"];
+  for (const field of requiredFields) {
+    if (!req.body[field] || typeof req.body[field] !== "string") {
+      res.status(HttpCode.BadRequest).send(`Missing ${field}`);
+    }
   }
-  const code = req.query["code"];
-  const data = oauth.exchangeCode(code);
+
+  const { code, grant_type, redirect_uri, client_id } = req.body;
+  const data = await oauth.exchangeCode(code, client_id, redirect_uri, grant_type);
   res.status(HttpCode.Good).json(data);
 });
--- example/server/src/services/oauth.ts
+++ example/server/src/services/oauth.ts
@@ -1,9 +1,52 @@
-// TODO: exchange code for token
-// TODO: use token to fetch user data
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const exchangeCode = (code: string) => {
-  const data = {};
-  return data;
+import jwt from "jsonwebtoken";
+
+interface TokenPayload {
+  access_token: string;
+  expires_in: number;
+  scope: string;
+  token_type: string;
+  id_token: string;
+}
+
+interface IdTokenPayload {
+  iss: string;
+  azp: string;
+  aud: string;
+  sub: string;
+  email: string;
+  email_verified: boolean;
+  at_hash: string;
+  name: string;
+  picture: string;
+  given_name: string;
+  family_name: string;
+  iat: number;
+  exp: number;
+}
+
+const clientSecret = process.env["GOOGLE_CLIENT_SECRET"] as string;
+
+const exchangeCode = async (
+  code: string,
+  grantType: string,
+  clientId: string,
+  redirectUri: string
+) => {
+  const url = new URL("https://oauth2.googleapis.com/token");
+  url.searchParams.set("grant_type", grantType);
+  url.searchParams.set("code", code);
+  url.searchParams.set("client_id", clientId);
+  url.searchParams.set("redirect_uri", redirectUri);
+  url.searchParams.set("client_secret", clientSecret);
+
+  fetch(url, { method: "POST" })
+    .then(res => res.json())
+    .then(json => {
+      const { id_token: idToken } = json as TokenPayload;
+
+      const decoded = jwt.decode(idToken) as IdTokenPayload;
+      return decoded;
+    });
 };
 
 export default {
