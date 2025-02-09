ee391c0a46fec1d8274bbaca6ebb5d193e054656
Enzo Falone <enzofalone123@gmail.com>
2025-02-08 22:08:49 -0500

    implemented /oauth/exchange, added env vars

--- example/server/src/index.ts
+++ example/server/src/index.ts
@@ -1,10 +1,13 @@
+import "dotenv/config";
 import express, { json } from "express";
+import cors from "cors";
 import attachRouters from "./routers/index.js";
 import { HttpCode } from "./util/http-code.js";
 
 const PORT = process.env["PORT"] || "3000";
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 attachRouters(app);
