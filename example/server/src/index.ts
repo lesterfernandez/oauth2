c69d96b763b8772e41fb36b95cb98696065b0a91
Enzo Falone <enzofalone123@gmail.com>
2025-03-03 18:04:40 -0500

    converted oauth class into module object

--- example/server/src/index.ts
+++ example/server/src/index.ts
@@ -1,15 +1,28 @@
 import "dotenv/config";
+import OAuth from "./middleware/oauth.js";
 import express, { json } from "express";
 import cors from "cors";
 import attachRouters from "./routers/index.js";
 import { HttpCode } from "./util/http-code.js";
 
 const PORT = process.env["PORT"] || "3000";
+const GOOGLE_CLIENT_SECRET = process.env["GOOGLE_CLIENT_SECRET"] as string;
+
+OAuth.setupProvider("google", {
+  baseUrl: "https://oauth2.googleapis.com/token",
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  onSuccess: ({ res, data }) => {
+    res.status(200).json(data);
+  },
+  onFailure: ({ res, error }) => {
+    res.status(500).json({ error: error.message });
+  },
+});
+
 const app = express();
 
 app.use(cors());
 app.use(json());
-
 attachRouters(app);
 
 app.use((_, res) => {
--- example/server/src/routers/oauth-router.ts
+++ example/server/src/routers/oauth-router.ts
@@ -1,19 +1,8 @@
 import { Router } from "express";
 import OAuth from "../middleware/oauth.js";
 
-export const oauthRouter = Router();
+const oauthRouter = Router();
 
-const googleClientSecret = process.env["GOOGLE_CLIENT_SECRET"] as string;
-const oauth = new OAuth();
-oauth.setupProvider("google", {
-  baseUrl: "https://oauth2.googleapis.com/token",
-  clientSecret: googleClientSecret,
-  onSuccess: ({ res, data }) => {
-    res.status(200).json(data);
-  },
-  onFailure: ({ res, error }) => {
-    res.status(500).json({ error: error.message });
-  },
-});
+oauthRouter.post("/exchange", OAuth.authenticate("google"));
 
-oauthRouter.post("/exchange", oauth.authenticate("google"));
+export default oauthRouter;
